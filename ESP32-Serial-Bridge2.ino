6320ec7e5d47d86f8df26df2f11e9f03f2057e08
Benjamin Choy <bench2004@gmail.com>
2020-08-08 12:50:37 +0800

    Add files via upload

---
+++ ESP32-Serial-Bridge2.ino
@@ -0,0 +1,302 @@
+// ESP32 WiFi <-> 3x UART Bridge
+// by AlphaLima
+// www.LK8000.com
+
+// Disclaimer: Don't use  for life support systems
+// or any other situations where system failure may affect
+// user or environmental safety.
+
+#include "config.h"
+#include <esp_wifi.h>
+#include <WiFi.h>
+#include <ESPmDNS.h>
+#include <WebServer.h>
+
+#ifdef BLUETOOTH
+#include <BluetoothSerial.h>
+BluetoothSerial SerialBT; 
+#endif
+
+#ifdef OTA_HANDLER  
+#include <ArduinoOTA.h> 
+
+#endif // OTA_HANDLER
+
+HardwareSerial Serial_one(1);
+HardwareSerial Serial_two(2);
+HardwareSerial* COM[NUM_COM] = {&Serial, &Serial_one , &Serial_two};
+
+#define MAX_NMEA_CLIENTS 4
+#ifdef PROTOCOL_TCP
+#include <WiFiClient.h>
+WiFiServer servercom_0(SERIAL0_TCP_PORT);
+WiFiServer servercom_1(SERIAL1_TCP_PORT);
+WiFiServer servercom_2(SERIAL2_TCP_PORT);
+WiFiServer *servercom[NUM_COM]={&servercom_0,&servercom_1,&servercom_2};
+WiFiClient TCPClient[NUM_COM][MAX_NMEA_CLIENTS];
+#endif
+const int controlPin = 2;
+bool poweredUp = false;
+WebServer server(80);
+
+uint8_t buf1[NUM_COM][bufferSize];
+uint16_t i1[NUM_COM]={0,0,0};
+
+uint8_t buf2[NUM_COM][bufferSize];
+uint16_t i2[NUM_COM]={0,0,0};
+
+uint8_t BTbuf[bufferSize];
+uint16_t iBT =0;
+
+
+void handleRoot()
+{
+    String response ="<html>";
+    response += "<head><title>Power up the AtomicPI!</title></head>\n";
+    response += "<body><h1>Power up the AtomicPI!</h1><div style=\"font-size:48px\"><p>Current status: ";
+    response += poweredUp?"<b><font color=\"red\">ON</font></b>":"OFF";
+    if (!poweredUp) {
+        response += "<p>Click <a href=\"/poweron\">here</a> to power up";
+    } else {
+        response += "<p>Click <a href=\"/poweroff\">here</a> to power off";
+    } 
+
+    response += "</div>";
+    server.send(200, "text/html", response);
+}
+
+void handleNotFound()
+{
+    String message = "File Not Found\n\n";
+    message += "URI: ";
+    message += server.uri();
+    message += "\nMethod: ";
+    message += (server.method() == HTTP_GET)?"GET":"POST";
+    message += "\nArguments: ";
+    message += server.args();
+    message += "\n";
+    for (uint8_t i=0; i<server.args(); i++){
+        message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+    }
+    server.send(404, "text/plain", message);
+}
+
+void setupWebServer(void)
+{
+    pinMode(controlPin, OUTPUT);
+    digitalWrite(controlPin, 0);
+  
+    if (MDNS.begin("esp32")) {
+        Serial.println("MDNS responder started");
+    }
+  
+    server.on("/", handleRoot);
+  
+    auto redirect = []() {
+        server.sendHeader("Location", String("http://") + WiFi.localIP() + String("/"), true);
+        server.send(302, "text/plain", "");  
+    };
+    server.on("/poweron", [&]() {
+        if (!poweredUp) {
+            poweredUp = true;
+            digitalWrite(controlPin, 1);
+        }
+        redirect();
+    });
+    server.on("/poweroff", [&]() {
+        if (poweredUp) {
+            poweredUp = false;
+            digitalWrite(controlPin, 0);
+        }
+        redirect();
+    });
+  
+    server.onNotFound(handleNotFound);
+  
+    server.begin();
+//    Serial.println("HTTP server started");
+}
+
+
+void setup() {
+
+  delay(500);
+    pinMode(controlPin, OUTPUT);
+    digitalWrite(controlPin, 0);
+  
+  COM[0]->begin(UART_BAUD0, SERIAL_PARAM0, SERIAL0_RXPIN, SERIAL0_TXPIN);
+  COM[1]->begin(UART_BAUD1, SERIAL_PARAM1, SERIAL1_RXPIN, SERIAL1_TXPIN);
+  COM[2]->begin(UART_BAUD2, SERIAL_PARAM2, SERIAL2_RXPIN, SERIAL2_TXPIN);
+  
+  if(debug) COM[DEBUG_COM]->println("\n\nLK8000 WiFi serial bridge V1.00");
+  #ifdef MODE_AP 
+   if(debug) COM[DEBUG_COM]->println("Open ESP Access Point mode");
+  //AP mode (phone connects directly to ESP) (no router)
+  WiFi.mode(WIFI_AP);
+   
+  WiFi.softAP(ssid, pw); // configure ssid and password for softAP
+  delay(2000); // VERY IMPORTANT
+  WiFi.softAPConfig(ip, ip, netmask); // configure ip address for softAP
+
+  #endif
+
+
+  #ifdef MODE_STA
+   if(debug) COM[DEBUG_COM]->println("Open ESP Station mode");
+  // STATION mode (ESP connects to router and gets an IP)
+  // Assuming phone is also connected to that router
+  // from RoboRemo you must connect to the IP of the ESP
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, pw);
+  if(debug) COM[DEBUG_COM]->print("try to Connect to Wireless network: ");
+  if(debug) COM[DEBUG_COM]->println(ssid);
+  while (WiFi.status() != WL_CONNECTED) {   
+    delay(500);
+    if(debug) COM[DEBUG_COM]->print(".");
+  }
+  if(debug) COM[DEBUG_COM]->println("\nWiFi connected");
+  
+  #endif
+#ifdef BLUETOOTH
+  if(debug) COM[DEBUG_COM]->println("Open Bluetooth Server");  
+  SerialBT.begin(ssid); //Bluetooth device name
+ #endif
+#ifdef OTA_HANDLER  
+  ArduinoOTA
+    .onStart([]() {
+      String type;
+      if (ArduinoOTA.getCommand() == U_FLASH)
+        type = "sketch";
+      else // U_SPIFFS
+        type = "filesystem";
+
+      // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+      Serial.println("Start updating " + type);
+    })
+    .onEnd([]() {
+      Serial.println("\nEnd");
+    })
+    .onProgress([](unsigned int progress, unsigned int total) {
+      Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+    })
+    .onError([](ota_error_t error) {
+      Serial.printf("Error[%u]: ", error);
+      if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+      else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+      else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+      else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+      else if (error == OTA_END_ERROR) Serial.println("End Failed");
+    });
+  // if DNSServer is started with "*" for domain name, it will reply with
+  // provided IP to all DNS request
+
+  ArduinoOTA.begin();
+#endif // OTA_HANDLER    
+
+  #ifdef PROTOCOL_TCP
+  COM[0]->println("Starting TCP Server 1");  
+  if(debug) COM[DEBUG_COM]->println("Starting TCP Server 1");  
+  servercom[0]->begin(); // start TCP server 
+  servercom[0]->setNoDelay(true);
+  COM[1]->println("Starting TCP Server 2");
+  if(debug) COM[DEBUG_COM]->println("Starting TCP Server 2");  
+  servercom[1]->begin(); // start TCP server 
+  servercom[1]->setNoDelay(true);
+  COM[2]->println("Starting TCP Server 3");
+  if(debug) COM[DEBUG_COM]->println("Starting TCP Server 3");  
+  servercom[2]->begin(); // start TCP server   
+  servercom[2]->setNoDelay(true);
+  #endif
+
+  esp_err_t esp_wifi_set_max_tx_power(50);  //lower WiFi Power
+  setupWebServer();
+}
+
+
+void loop() 
+{  
+  
+#ifdef OTA_HANDLER  
+  ArduinoOTA.handle();
+#endif // OTA_HANDLER
+  
+#ifdef BLUETOOTH
+  // receive from Bluetooth:
+  if(SerialBT.hasClient()) 
+  {
+    while(SerialBT.available())
+    {
+      BTbuf[iBT] = SerialBT.read(); // read char from client (LK8000 app)
+      if(iBT <bufferSize-1) iBT++;
+    }          
+    for(int num= 0; num < NUM_COM ; num++)
+      COM[num]->write(BTbuf,iBT); // now send to UART(num):          
+    iBT = 0;
+  }  
+#endif  
+#ifdef PROTOCOL_TCP
+  for(int num= 0; num < NUM_COM ; num++)
+  {
+    if (servercom[num]->hasClient())
+    {
+      for(byte i = 0; i < MAX_NMEA_CLIENTS; i++){
+        //find free/disconnected spot
+        if (!TCPClient[num][i] || !TCPClient[num][i].connected()){
+          if(TCPClient[num][i]) TCPClient[num][i].stop();
+          TCPClient[num][i] = servercom[num]->available();
+          if(debug) COM[DEBUG_COM]->print("New client for COM"); 
+          if(debug) COM[DEBUG_COM]->print(num); 
+          if(debug) COM[DEBUG_COM]->println(i);
+          continue;
+        }
+      }
+      //no free/disconnected spot so reject
+      WiFiClient TmpserverClient = servercom[num]->available();
+      TmpserverClient.stop();
+    }
+  }
+#endif
+ 
+  for(int num= 0; num < NUM_COM ; num++)
+  {
+    if(COM[num] != NULL)          
+    {
+   
+      for(byte cln = 0; cln < MAX_NMEA_CLIENTS; cln++)
+      {               
+        if(TCPClient[num][cln]) 
+        {
+          while(TCPClient[num][cln].available())
+          {
+            buf1[num][i1[num]] = TCPClient[num][cln].read(); // read char from client (LK8000 app)
+            if(i1[num]<bufferSize-1) i1[num]++;
+          } 
+          COM[num]->write(buf1[num], i1[num]); // now send to UART(num):
+          i1[num] = 0;
+        }
+      }
+  
+      if(COM[num]->available())
+      {
+        while(COM[num]->available())
+        {     
+          buf2[num][i2[num]] = COM[num]->read(); // read char from UART(num)
+          if(i2[num]<bufferSize-1) i2[num]++;
+        }
+        // now send to WiFi:
+        for(byte cln = 0; cln < MAX_NMEA_CLIENTS; cln++)
+        {   
+          if(TCPClient[num][cln])                     
+            TCPClient[num][cln].write(buf2[num], i2[num]);
+        }
+#ifdef BLUETOOTH        
+        // now send to Bluetooth:
+        if(SerialBT.hasClient())      
+          SerialBT.write(buf2[num], i2[num]);               
+#endif  
+        i2[num] = 0;
+      }
+    }    
+  }
+  server.handleClient();
+}
